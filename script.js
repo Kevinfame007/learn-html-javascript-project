2bb226b84ca1c2dfec4621aa07a9f8caf5723ad7
Kevinfame007 <kevinfame007@gmail.com>
2021-11-13 22:33:44 +0700

    learn how to use Array method slice in JavaScript
    learn how to use Array method slice in JavaScript

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-// learn how to use method arrays in JavaScript
+// learn how to use Array method slice in JavaScript
 
-var fruits = [
-    "Banana",
-    "Orange",
-    "Apple",
-    "Mango"
-];
+// var arr1 = ["value1", "value2"];
+// // var arr2 = arr1.concat(["value3", "value4", "value5"]);
+// var arr2 = ["value3", "value4", "value5"];
+// var arr3 = ["value6", "value7"];
 
-// document.getElementById("demo").innerHTML = fruits.toString();
+// var concatArr = arr1.concat(arr2, arr3);
 
-// document.getElementById("demo").innerHTML = fruits.join(" - ");
+var arr1 = ["value1", "value2", "value3", "value4", "value5"];
 
-// fruits.pop();
-// fruits.push("Kiwi");
-// fruits.shift();
-// fruits.unshift("Lemon");
-
-// fruits[0] = "Kiwi";
-
-// delete fruits[0];
-// fruits.splice(2, 0, "Lemon", "Kiwi");
-fruits.splice(0, 2);
-
-console.log(fruits);
-
+var myNewArr = arr1.slice(1, 3);
