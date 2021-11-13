44fb0682e99979443f1161ea3040c03d391ddac3
Kevinfame007 <kevinfame007@gmail.com>
2021-11-13 16:14:16 +0700

    learn JavaScript Statement
    learn JavaScript Statement

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello, World");
+
+document.getElementById("text").innerHTML = "Suphanut Taengtoemphon";
+
+document.write("Hello Document Write");
+
+window.alert("Hello Alert Box");
+
