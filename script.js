2a26873485d55f703d423597e607952610aaf463
Kevinfame007 <kevinfame007@gmail.com>
2021-11-13 21:48:12 +0700

    learn how to work with string in JavaScript
    learn how to work with string in JavaScript

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-// learn how to use JavaScript Events
+// learn how to work with string in JavaScript
 
+var x = "Suphanut \"fame\" Taengtoemphon";
+var y = 'Suphanut \'fame\' Taengtoemphon';
+var z = 'Suphanut \\ \'fame\' \\ Taengtoemphon';
+var a = 'It\'s me.';
+var b = 'lorem alsdkf;nnnnnnnnnnnnnnnnnnnnnnnn \ndddddddddddasdaswdsafalskflasjklfa;sd'
+
+console.log(b);
+console.log(a);
+console.log(z);
+console.log(y);
+console.log(x);
+console.log(x.length);
