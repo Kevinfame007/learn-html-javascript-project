f405515248e7a7178612c73d59d934d25fd61da4
Kevinfame007 <kevinfame007@gmail.com>
2021-11-14 20:18:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // learn how to use JavaScript For loop
 
+// 1
 // for(var i = 0; i <= 5; i++) {
 //     console.log(i);
 // }
@@ -15,5 +16,38 @@
 //     console.log(cars[i]);
 // }
 
-//For in loop
+//----------------------------------------------
+// 2
+// For in loop
 
+// var person = {
+//     fname: "Suphanut",
+//     lname: "Taengtoemphon",
+//     age: 23
+// };
+
+// for (var x in person) {
+//     console.log(person[x]);
+// }
+
+//-----------------------------------------------
+// 3
+// For of Loop
+
+var cars = [
+    "BMW",
+    "Ford",
+    "Honda",
+];
+
+for (var x of cars) {
+    console.log(x);
+}
+
+var myString = "JavaScript";
+
+var y;
+
+for (y of myString) {
+    console.log(y);
+}
